c01acfdf55bf0aa83c427ac0499c8edbe4d7ce93
maleman <snax2251@gmail.com>
2024-03-15 09:48:45 -0600

    Fix responsive vista de blog

--- src/main/java/sv/bandesal/app/service/BlogService.java
+++ src/main/java/sv/bandesal/app/service/BlogService.java
@@ -12,29 +12,40 @@ import sv.bandesal.app.repository.BlogRepository;
 
 @Service
 public class BlogService {
-	
+
 	private BlogRepository blogRepository;
-	
+
 	public BlogService(BlogRepository blogRepository) {
 		this.blogRepository = blogRepository;
 	}
-	
+
 	public void saveBlog(Blog blog) {
 		blogRepository.save(blog);
 	}
-	
+
 	public void deleteBlog(int id) {
 		Optional<Blog> blog = blogRepository.findById(id);
-		if(blog.isPresent()) {
+		if (blog.isPresent()) {
 			blogRepository.delete(blog.get());
 		}
 	}
-	
+
 	public Blog getBlogById(int id) {
 		return blogRepository.findById(id).orElse(null);
 	}
-	
-	public List<Blog> getAllBlogs(){
-		return StreamSupport.stream(blogRepository.findAll().spliterator(), false).collect(Collectors.toList());
+
+	public List<Blog> getAllBlogs() {
+		List<Blog> lista = StreamSupport.stream(blogRepository.findAll().spliterator(), false)
+				.collect(Collectors.toList());
+
+		lista = lista.stream().map(blog -> {
+			if(blog != null && blog.getDescription() != null && blog.getDescription().length() > 400) {
+				blog.setDescription(blog.getDescription().substring(0, 400)+" ...");
+			}
+		
+			return blog;
+		}).collect(Collectors.toList());
+		
+		return lista;
 	}
 }
